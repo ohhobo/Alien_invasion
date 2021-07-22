3d8cafe15e2d3c55af109b785db8c91952bdfccf
oh-hobo <cyr010523@163.com>
2021-07-22 18:12:25 +0800

    day 1.

---
+++ alien_invasion.py
@@ -0,0 +1,64 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+class AlienInvasion():
+    #管理游戏资源和行为的类
+    def __init__(self):
+        #初始化游戏并创建游戏资源
+        pygame.init()
+        self.settings=Settings()
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width=self.screen.get_rect().width
+        self.settings.screen_height=self.screen.get_rect().height
+        pygame.display.set_caption('Alien Invasion')
+
+        self.ship=Ship(self)
+
+    def run_game(self):
+        #开始游戏的主循环
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+
+    def _check_events(self):
+        # 监控鼠标和键盘事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                 sys.exit()
+            elif event.type==pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type==pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self,event):
+        #响应按键
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self,event):
+        # 响应松开
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+
+    def _update_screen(self):
+        # 每次循环时都会重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+if __name__=='__main__':
+    #创建游戏实例并运行游戏:
+    ai=AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    #存储游戏中的所有设置的类
+    def __init__(self):
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
+        #飞船设置
+        self.ship_speed=1.5
